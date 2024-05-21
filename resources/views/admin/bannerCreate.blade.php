d1cd6c5d4876a57b4f83e64f908476819a503aa2
BaekJinYoung <time121412@gmail.com>
2024-05-21 15:50:08 +0900

    feat: admin banner 등록 후 계속(등록 미완)

--- resources/views/admin/bannerCreate.blade.php
+++ resources/views/admin/bannerCreate.blade.php
@@ -12,52 +12,53 @@
 
             <div class="title-wrap col-group">
                 <h2 class="main-title">
-                    메인 배너 등록
+                    배너 등록
                 </h2>
             </div>
-            <form enctype="multipart/form-data">
+            <form action="{{route('admin.bannerStore')}}" method="post" enctype="multipart/form-data">
+                @csrf
                 <div class="form-wrap row-group">
                     <div class="form-item row-group">
                         <p class="item-default">
                             제목
                             <span class="red">*</span>
                         </p>
-                        <textarea rows="2" name="title" placeholder="제목을 작성해주세요."></textarea>
+                        <input type="text" name="title" class="form-input" id="title" placeholder="제목을 입력하세요">
                     </div>
                     <div class="form-item row-group">
                         <p class="item-default">
                             MOBILE 제목
                             <span class="red">*</span>
                         </p>
-                        <textarea rows="2" name="mb_title" placeholder="MOBILE 제목을 작성해주세요."></textarea>
+                        <input type="text" name="mobile_title" class="form-input" id="mobile_title" placeholder="제목을 입력하세요">
                     </div>
                     <div class="form-item row-group">
                         <p class="item-default">
                             부제목
                             <span class="red">*</span>
                         </p>
-                        <textarea rows="2" name="sub_title" placeholder="부제목을 작성해주세요."></textarea>
+                        <input type="text" name="subtitle" class="form-input" id="subtitle" placeholder="제목을 입력하세요">
                     </div>
                     <div class="form-item row-group">
                         <p class="item-default">
                             MOBILE 부제목
                             <span class="red">*</span>
                         </p>
-                        <textarea rows="2" name="mb_sub_title" placeholder="MOBILE 부제목을 작성해주세요."></textarea>
+                        <input type="text" name="mobile_subtitle" class="form-input" id="mobile_subtitle" placeholder="제목을 입력하세요">
                     </div>
                     <div class="form-item row-group">
                         <p class="item-default">
                             내용
                             <span class="red">*</span>
                         </p>
-                        <textarea rows="3" name="content" placeholder="내용을 작성해주세요."></textarea>
+                        <input type="text" name="details" class="form-input" id="details" placeholder="제목을 입력하세요">
                     </div>
                     <div class="form-item row-group">
                         <p class="item-default">
                             MOBILE 내용
                             <span class="red">*</span>
                         </p>
-                        <textarea rows="3" name="mb_content" placeholder="MOBILE 내용을 작성해주세요."></textarea>
+                        <input type="text" name="mobile_details" class="form-input" id="mobile_details" placeholder="제목을 입력하세요">
                     </div>
                     <div class="form-item row-group">
                         <p class="item-default">
@@ -65,15 +66,16 @@
                             <span class="red">*</span>
                         </p>
                         <div class="file-upload-wrap">
-                            <input type='file' id='pc_file_upload' accept="image/*" name="pc_file">
-                            <label for="pc_file_upload" class="file-upload-btn">
+                            <!-- <input type='file' id='popup_file' accept="image/*" name="image"> -->
+                            <input type='file' id='image' accept="image/*" name="image">
+                            <label for="popup_file" class="file-upload-btn">
                                 파일 업로드
                             </label>
                             <span class="guide-txt">
-                                1920*980px 비율 고해상도 사진 등록
+                                800*800px 비율 고해상도 사진 등록
                             </span>
 
-                            <div class="file-preview" id="pc_file">
+                            <div class="file-preview" id="image">
                                 <p class="file-name"></p>
                             </div>
                         </div>
@@ -84,33 +86,33 @@
                             <span class="red">*</span>
                         </p>
                         <div class="file-upload-wrap">
-                            <input type='file' id='mb_file_upload' accept="image/*" name="mb_file">
-                            <label for="mb_file_upload" class="file-upload-btn">
+                            <!-- <input type='file' id='popup_file' accept="image/*" name="image"> -->
+                            <input type='file' id='mobile_image' accept="image/*" name="mobile_image">
+                            <label for="popup_file" class="file-upload-btn">
                                 파일 업로드
                             </label>
                             <span class="guide-txt">
-                                960*2080px 비율 고해상도 사진 등록
+                                800*800px 비율 고해상도 사진 등록
                             </span>
 
-                            <div class="file-preview" id="mb_file">
+                            <div class="file-preview" id="mobile_image">
                                 <p class="file-name"></p>
                             </div>
                         </div>
                     </div>
                 </div>
-
+                <div class="form-btn-wrap col-group">
+                    <a href="{{route("admin.bannerIndex")}}" class="form-prev-btn">
+                        목록으로
+                    </a>
+                    <button class="form-prev-btn" type="submit">
+                        등록
+                    </button>
+                    <button class="form-submit-btn" name="continue" type="submit">
+                        등록 후 계속
+                    </button>
+                </div>
             </form>
-            <div class="form-btn-wrap col-group">
-                <a href="{{route("admin.bannerIndex")}}" class="form-prev-btn">
-                    목록으로
-                </a>
-                <button class="form-prev-btn" type="submit">
-                    등록
-                </button>
-                <button class="form-submit-btn" type="submit">
-                    등록 후 계속
-                </button>
-            </div>
         </div>
     </div>
 </div>
