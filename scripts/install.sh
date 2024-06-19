b30379e55a058218c41821e3b2601b195a400a07
BaekJinYoung <time121412@gmail.com>
2024-06-19 17:19:01 +0900

    chore: 깃 액션-mysql acesss

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,7 +54,8 @@ secure_mysql_installation() {
     sudo systemctl start mysql
 
     # MySQL root 비밀번호 설정
-    sudo mysql -u root -p "${MYSQL_ROOT_PASSWORD}" <<-EOF
+    sudo mysql -u root -p  <<-EOF
+    "${MYSQL_ROOT_PASSWORD}"
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
     FLUSH PRIVILEGES;
 EOF
